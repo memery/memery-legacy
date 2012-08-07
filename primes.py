b119ad0d59bcfa945253cf486d33b1d687710713
kqr <k@rdw.se>
2012-08-07 19:15:39 +0200

    portat primes.php

---
+++ primes.py
@@ -0,0 +1,35 @@
+
+
+def help():
+  raise NotImplementedError
+
+def run(nick, args):
+  return "{0}: {1}".format(nick, primes(args))
+
+
+def primes(args):
+  try:
+    number = int(args)
+    if number < 2 or number > 99999:
+      raise ValueError
+
+  except ValueError:
+    return "Argument must be an integer between 2 and not too high!"
+
+  return "{0} = {1}".format(number, ' * '.join([str(f) for f in factorise(number)]))
+
+
+def factorise(number):
+  factors = []
+  factor = 0
+
+  while factor < number:
+    factor = 2
+    while number % factor != 0:
+      factor += 1
+    number /= factor
+    factors.append(factor)
+
+  return factors
+
+
