dd3e86a377560d78b76c04cc5a13b1c0f34736bc
kqr <k@rdw.se>
2012-08-26 02:49:44 +0200

    gjorde gcd.py och primes.py snyggare

--- plugins/gcd.py
+++ plugins/gcd.py
@@ -1,28 +1,25 @@
 
 def help():
-  return {'authors':     ['kqr'],
-          'years':       ['2012'],
-          'version':     '1.0',
-          'description': 'Calculates the greatest common divisor of two natural numbers.',
-          'argument':    '<natural number> <natural number>'}
+    return {'authors':     ['kqr'],
+            'years':       ['2012'],
+            'version':     '1.0',
+            'description': 'Calculates the greatest common divisor of two natural numbers.',
+            'argument':    '<natural number> <natural number>'}
 
 def run(nick, args):
-  return "{0}: {1}".format(nick, gcd(args))
+    try:
+        a, b = [int(n) for n in args.split()]
+    except ValueError:
+        return "Specify two integers, space separated."
 
-def gcd(args):
-  try:
-    a, b = [int(n) for n in args.split()]
-  except ValueError:
-    return "Specify two integers, space separated."
+    return "{}: GCD({}, {}) = {}".format(nick, a, b, gcd(a, b))
 
-  return "GCD({0}, {1}) = {2}".format(a, b, euclids(a, b))
-
-def euclids(a, b):
-  rest = -1
-  while rest != 0:
-    rest = a - a/b * b
-    a = b
-    b = rest
-  return a
+def gcd(a, b):
+    rest = -1
+    while rest != 0:
+        rest = a - int(a/b) * b
+        a = b
+        b = rest
+    return a
 
 
