3f3eccab2df9f90ea5bc046f40b7f71910d2893e
kqr <k@rdw.se>
2012-09-09 14:01:20 +0200

    lade till variabel för startuptime i memery.py

--- memery.py
+++ memery.py
@@ -1,13 +1,15 @@
 from imp import reload
+from time import time
 import common, irc
 
 def main():
+    startuptime = time()
     # Crash like hell if the config is crap!
     settings = common.read_config('config')
     # Kind of like state
     message = ''
     while True:
-        result = irc.run(message, settings)
+        result = irc.run(message, settings, startuptime)
         message = ''
         if result == 'reconnect':
             continue
